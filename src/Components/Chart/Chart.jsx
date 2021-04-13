cc5566c80b812715b3a2fbd86aafaa2f0def4dc1
Muhammad Waleed <dev-waleed@outlook.com>
2021-04-13 10:12:06 +0500

    Initial Cleanup and Setup

---
+++ src/Components/Chart/Chart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Chart = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+export default Chart;
---
+++ src/Components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CountryPicker = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+export default CountryPicker;
